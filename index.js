baf704fa26c8a6fb397a132b2b308770b2c361c7
unknown <PrinceRohith>
2025-04-30 01:43:29 +0530

    first

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mysql = require('mysql2/promise');
+const fs = require('fs');
+const app = express();
+app.use(express.json());
+
+// TiDB Cloud connection
+const pool = mysql.createPool({
+  host: 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com',
+    user: 'rChdBcmPh6AhXhn.root',
+    password: '3kmCMWHZkkhXHH35',
+    database: 'test',
+    ssl: {
+      ca: fs.readFileSync('./ca.pem'),
+      rejectUnauthorized: true,
+    },
+});
+
+app.post('/api/sensor-data', async (req, res) => {
+  const { co, co2, temp, humidity } = req.body;
+  try {
+    const conn = await pool.getConnection();
+    await conn.execute(
+      'INSERT INTO sensor_data (co, co2, temperature, humidity) VALUES (?, ?, ?, ?)',
+      [co, co2, temp, humidity]
+    );
+    conn.release();
+    res.sendStatus(200);
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
